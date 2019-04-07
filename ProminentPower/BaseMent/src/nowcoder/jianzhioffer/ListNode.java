745ca40bc6e788b0c2646677616b28ad5e8d31bc
MikeYoung1995 <Mike24KE@Gmail.com>
2019-04-07 09:20:56 +0800

    替换空格

---
+++ ProminentPower/BaseMent/src/nowcoder/jianzhioffer/ListNode.java
@@ -0,0 +1,15 @@
+package nowcoder.jianzhioffer;
+
+/**
+ * 输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。
+ */
+
+class ListNode {
+    int val;
+    ListNode next =null;
+    ListNode(int val)
+    {
+        this.val=val;
+
+    }
+}
---
+++ ProminentPower/BaseMent/src/nowcoder/jianzhioffer/Solution002.java
@@ -0,0 +1,37 @@
+package nowcoder.jianzhioffer;
+//替换空格
+/**
+ * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+ * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ */
+public class Solution002 {
+
+    /**
+     *  StringBuffer和StringBuilder的区别
+     * @param str
+     * @return
+     */
+    public static String replaceSpace(StringBuffer str) {
+        StringBuffer stringBuffer =new StringBuffer();
+    for(int i=0;i<str.length();i++)
+    {
+        if(str.charAt(i)==' ')
+        {
+            stringBuffer.append('%');
+            stringBuffer.append('2');
+            stringBuffer.append('0');
+        }
+        else
+        {
+            stringBuffer.append(str.charAt(i));
+        }
+    }
+        return  stringBuffer.toString();
+    }
+    public static void main(String[] args)
+    {
+         StringBuffer stringBuffer =new StringBuffer("hello world");
+         System.out.println(replaceSpace(stringBuffer));
+
+    }
+}
